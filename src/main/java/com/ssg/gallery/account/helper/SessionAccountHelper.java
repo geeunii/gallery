199f01232a67463288201296fe9f8aefb2cf581c
thatzcool <mycaptain0771@gmail.com>
2025-11-24 15:31:59 +0900

    gallery_backend_02

--- src/main/java/com/ssg/gallery/account/helper/SessionAccountHelper.java
+++ src/main/java/com/ssg/gallery/account/helper/SessionAccountHelper.java
@@ -2,6 +2,9 @@ package com.ssg.gallery.account.helper;
 
 import com.ssg.gallery.account.dto.AccountJoinRequests;
 import com.ssg.gallery.account.dto.AccountLoginRequests;
+import com.ssg.gallery.account.etc.AccountConstants;
+import com.ssg.gallery.common.util.HttpUtils;
+import com.ssg.gallery.member.entity.Member;
 import com.ssg.gallery.member.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,26 +20,38 @@ public class SessionAccountHelper implements AccountHelper {
 
     @Override
     public void join(AccountJoinRequests joinReq) {
-
+             memberService.save(joinReq.getName(), joinReq.getLoginId(), joinReq.getLoginPw());
     }
 
     @Override
     public String login(AccountLoginRequests loginReq, HttpServletRequest request, HttpServletResponse response) {
-        return "";
+          Member member = memberService.find(loginReq.getLoginId() , loginReq.getLoginPw());
+
+          //회원 데이터가 없으면
+        if (member == null) {
+            return null;
+        }
+        HttpUtils.setSession(request, AccountConstants.Member_ID_NAME, member.getId());
+        return member.getLogin_id().toString();
+
     }
 
+    //회원 아이디 조회
     @Override
-    public Integer getMemberId(AccountLoginRequests loginReq) {
-        return 0;
+    public Integer getMemberId(HttpServletRequest request) {
+        Object memberId = HttpUtils.getSession(request,AccountConstants.Member_ID_NAME);
+        if(memberId != null) { return (int) memberId; }
+        return null;
     }
 
+    //로그인 여부 확인
     @Override
     public boolean isLoggedIn(HttpServletRequest request) {
-        return false;
+        return getMemberId(request) != null;
     }
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response) {
-
+           HttpUtils.removeSession(request,AccountConstants.Member_ID_NAME);
     }
 }
