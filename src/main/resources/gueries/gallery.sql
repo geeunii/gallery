1ed538f8f8a2f7154dff2b55242d09a68e505610
KHG <koo4934@gmail.com>
2025-12-05 12:33:04 +0900

    Docker로 연결하고 실습하기 위함

--- src/main/resources/gueries/gallery.sql
+++ src/main/resources/gueries/gallery.sql
@@ -1,56 +1,68 @@
 use gallery;
 
+create schema if not exists gallery;
+
+
+drop schema if exists gallery;
+
+
 drop table order_items;
 drop table orders;
 
-create table blocks (
-                        id      int auto_increment comment '아이디' primary key,
-                        token   varchar(250)                         not null comment '차단 토큰',
-                        created datetime default current_timestamp() not null comment '생성 일시'
+create table blocks
+(
+    id      int auto_increment comment '아이디' primary key,
+    token   varchar(250)                         not null comment '차단 토큰',
+    created datetime default current_timestamp() not null comment '생성 일시'
 ) comment '토큰 차단';
 
-create table carts (
-                       id        int auto_increment comment '아이디' primary key,
-                       member_id int                                  not null comment '회원 아이디',
-                       item_id   int                                  not null comment '상품 아이디',
-                       created   datetime default current_timestamp() not null comment '생성 일시'
+create table carts
+(
+    id        int auto_increment comment '아이디' primary key,
+    member_id int                                  not null comment '회원 아이디',
+    item_id   int                                  not null comment '상품 아이디',
+    created   datetime default current_timestamp() not null comment '생성 일시'
 ) comment '장바구니';
 
-create table items (
-                       id           int auto_increment comment '아이디' primary key,
-                       name         varchar(50)                          not null comment '상품 이름',
-                       img_path     varchar(100)                         not null comment '상품 사진 경로',
-                       price        int                                  not null comment '상품 가격',
-                       discount_per int                                  not null comment '상품 할인율',
-                       created      datetime default current_timestamp() not null comment '생성 일시'
+create table items
+(
+    id           int auto_increment comment '아이디' primary key,
+    name         varchar(50)                          not null comment '상품 이름',
+    img_path     varchar(100)                         not null comment '상품 사진 경로',
+    price        int                                  not null comment '상품 가격',
+    discount_per int                                  not null comment '상품 할인율',
+    created      datetime default current_timestamp() not null comment '생성 일시'
 ) comment '상품';
 
-create table members (
-                         id            int auto_increment comment '아이디' primary key,
-                         name          varchar(50)                          not null comment '회원명',
-                         login_id      varchar(50)                          not null comment '로그인 아이디',
-                         login_pw      char(44)                             not null comment '로그인 패스워드',
-                         login_pw_salt char(16)                             not null comment '로그인 패스워드 솔트',
-                         created       datetime default current_timestamp() not null comment '생성 일시',
-                         constraint members_uk unique (login_id)
+create table members
+(
+    id            int auto_increment comment '아이디' primary key,
+    name          varchar(50)                          not null comment '회원명',
+    login_id      varchar(50)                          not null comment '로그인 아이디',
+    login_pw      char(44)                             not null comment '로그인 패스워드',
+    login_pw_salt char(16)                             not null comment '로그인 패스워드 솔트',
+    created       datetime default current_timestamp() not null comment '생성 일시',
+    constraint members_uk unique (login_id)
 ) comment '회원';
 
-create table order_items (
-                             id       int auto_increment comment '아이디' primary key,
-                             order_id int                                  not null comment '주문 아이디',
-                             item_id  int                                  not null comment '주문 상품 아이디',
-                             created  datetime default current_timestamp() not null comment '생성 일시'
+create table order_items
+(
+    id       int auto_increment comment '아이디' primary key,
+    order_id int                                  not null comment '주문 아이디',
+    item_id  int                                  not null comment '주문 상품 아이디',
+    created  datetime default current_timestamp() not null comment '생성 일시'
 ) comment '주문 상품';
 
-create table orders (
-                        id          int auto_increment comment '아이디' primary key,
-                        member_id   int                                  not null comment '주문자 회원 아이디',
-                        name        varchar(50)                          not null comment '주문자명',
-                        address     varchar(500)                         not null comment '배송 주소',
-                        payment     varchar(10)                          not null comment '결제 수단',
-                        card_number varchar(50)                          null comment '카드 번호',
-                        amount      bigint                               not null comment '최종 결제 금액',
-                        created     datetime default current_timestamp() not null comment '생성 일시'
+create table orders
+(
+    id          int auto_increment comment '아이디' primary key,
+    member_id   int                                  not null comment '주문자 회원 아이디',
+    name        varchar(50)                          not null comment '주문자명',
+    address     varchar(500)                         not null comment '배송 주소',
+    payment     varchar(10)                          not null comment '결제 수단',
+    card_number varchar(50)                          null comment '카드 번호',
+    amount      bigint                               not null comment '최종 결제 금액',
+    created     datetime default current_timestamp() not null comment '생성 일시'
 ) comment '주문';
 
 INSERT INTO items (name, img_path, price, discount_per)
@@ -95,17 +107,27 @@ VALUES (3, '감마', '경기도 파주시 문발로 115', 'card', 27500000)
      , (3, '에타', '전라남도 여수시 해양로 3456', 'bank', 27500000);
 
 INSERT INTO order_items (order_id, item_id)
-SELECT id, 1 FROM orders;
+SELECT id, 1
+FROM orders;
 
 INSERT INTO order_items (order_id, item_id)
-SELECT id, 2 FROM orders;
+SELECT id, 2
+FROM orders;
 
-delete  from members where id =1;
-select * from members;
+delete
+from members
+where id = 1;
+select *
+from members;
 
-select * from items;
-select * from orders;
-select * from order_items;
-# insert into members(name, login_id,login_pw) values('알파','alpha@example.com','1111'),
-#                                                    ('베타','beta@example.com','1111'),
-#                                                    ('감마','gamma@example.com','1111');
+select *
+from items;
+select *
+from orders;
+select *
+from order_items;
+
+insert into members(name, login_id, login_pw, login_pw_salt)
+values ('알파', 'alpha@example.com', '1111', '1111'),
+       ('베타', 'beta@example.com', '1111', '1111'),
+       ('감마', 'gamma@example.com', '1111', '1111');
